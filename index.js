622e67ff830cace206b859312412ee413c8de62c
KAY S <KS.KWANRUAN@GMAIL.com>
2020-08-14 13:51:02 +1000

    change order of commands in Dockerfile and test the app

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hi There");
+  res.send("Hello! How are you doing?");
 });
 
 app.listen(8080, () => {
